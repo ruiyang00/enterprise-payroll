8860492a7a95523ae924603cb85502d45c285432
sanfordhuynh <huynhsanford@gmail.com>
2019-05-03 14:00:46 -0700

    some changes

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,81 +1,83 @@
-import React, { Component } from 'react'
-import { reduxForm, Field } from 'redux-form';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { compose } from "redux";
 
-import CustomInput from './CustomInput';
-import * as actions from '../actions';
-
+import CustomInput from "./CustomInput";
+import * as actions from "../actions";
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
+  async onSubmit(formData) {
+    console.log("onSubmit() got called");
+    console.log("formData", formData);
 
-    async onSubmit(formData) {
-        console.log('onSubmit() got called');
-        console.log('formData', formData);
+    //need to call some action
+    await this.props.signUp(formData);
+  }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <button type="submit" className="btn btn-outline-primary">
+          {" "}
+          Log in with Facebook
+        </button>
+        <button type="submit" className="btn btn-outline-primary">
+          Log in with Google
+        </button>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <fieldset>
+            <Field
+              name="email"
+              type="text"
+              id="email"
+              label="Enter your email"
+              placeholder="exmaple@example.com"
+              component={CustomInput}
+            />
+          </fieldset>
+          <fieldset>
+            <Field
+              name="password"
+              type="password"
+              id="password"
+              label="Enter your password"
+              placeholder="yourpassword"
+              component={CustomInput}
+            />
+          </fieldset>
 
-        //need to call some action
-        await this.props.signUp(formData);
+          {this.props.errorMessage ? (
+            <div className="alert alert-danger" />
+          ) : null}
 
-
-    }
-    render() {
-        const { handleSubmit } = this.props;
-        return (
-            <div>
-
-
-                <button type="submit" className="btn btn-outline-primary"> Log in with Facebook</button>
-                <button type="submit" className="btn btn-outline-primary">Log in with Google</button>
-                <form onSubmit={handleSubmit(this.onSubmit)}>
-                    <fieldset>
-                        <Field
-                            name="email"
-                            type="text"
-                            id="email"
-                            label="Enter your email"
-                            placeholder="exmaple@example.com"
-                            component={CustomInput} />
-
-                    </fieldset>
-                    <fieldset>
-                        <Field
-                            name="password"
-                            type="password"
-                            id="password"
-                            label="Enter your password"
-                            placeholder="yourpassword"
-                            component={CustomInput} />
-                    </fieldset>
-
-                    {this.props.errorMessage ?
-                        <div className="alert alert-danger">
-                            {a()}
-                        </div> : null}
-
-                    <button type="submit" className="btn btn-outline-primary">Sign Up</button>
-
-                </form>
-            </div>
-        )
-    }
+          <button type="submit" className="btn btn-outline-primary">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 function a() {
-    return console.log("hello");
+  return console.log("hello");
 }
-
 
 function mapStateToProps(state) {
-    return {
-        errorMessage: state.errorMessage
-    }
+  return {
+    errorMessage: state.errorMessage
+  };
 }
 export default compose(
-    connect(mapStateToProps, actions),
-    reduxForm({ form: 'signup' })
-)(SignUp)
+  connect(
+    mapStateToProps,
+    actions
+  ),
+  reduxForm({ form: "signup" })
+)(SignUp);
